652b169c3eb22b258d76a10a4550e24d7c5656a8
Jeroenb13 <jeroen.bakker@student.stenden.com>
2017-05-11 13:13:13 +0200

    begonnen met typescript google maps

--- src/app/components/location-page/location-page.ts
+++ src/app/components/location-page/location-page.ts
@@ -5,18 +5,5 @@ import { Component } from '@angular/core';
     styles: ['ul { color: blue; }']
 })
 export class LocationComponent{
-    public name: string;
-    private map: any;
-    private options: any;
 
-    constructor (mapDiv: Element)
-    {
-        this.name = "GoogleMap";
-        this.options =
-            {center: new google.maps.LatLng(1,1),
-                zoom: 3,
-                MapTypeId: google.maps.MapTypeID.TERRAIN
-            };
-        this.map = new google.maps.Map(mapDiv, this.options);
-    }
 }
