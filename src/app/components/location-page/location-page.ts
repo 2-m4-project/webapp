26a2cca39982d9659fada027bcb41ac96d867217
ElderMatt <matthew.elderhorst@hotmail.com>
2017-05-15 14:31:02 +0200

    Locatie pagina opdezelfde manier als Geschiedenis pagina

--- src/app/components/location-page/location-page.ts
+++ src/app/components/location-page/location-page.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AgmMap, AgmMarker } from '@agm/core';
 
 @Component({
     templateUrl: './location-page.html',
-    styles: ['ul { color: blue; }']
+    styles: ['ul { color: blue; }'],
+    styleUrls: ['./style.css'],
+    directives: [AgmMap]
 })
-export class LocationComponent{
+export class LocationComponent implements OnInit{
 
+    public lat: number;
+    public lng: number;
+    public zoom: number;
+
+    public markers: Marker[];
+
+    ngOnInit(): void {
+        this.markers = [
+            new Marker(52.7788414, 6.914114, 'M')
+        ];
+    }
+}
+
+    export class Marker {
+    public lat: number;
+    public long: number;
+    public marker: string;
+
+    constructor (lat: number, long: number, marker: string){
+        this.lat = lat;
+        this.long = long;
+        this.marker = marker;
+    }
 }
