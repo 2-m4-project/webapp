e933db284ff1ab70e33f1720660d68de39064874
DaanHaitsma <daan@haitsma-dejager.nl>
2017-05-15 12:54:58 +0200

    Straatnaam weergegeven op alarmeringen-pagina

--- src/app/components/alarmeringen-page/alarmeringen-page.ts
+++ src/app/components/alarmeringen-page/alarmeringen-page.ts
@@ -1,39 +1,51 @@
 import { Component } from '@angular/core';
+import {Http, Response} from '@angular/http';
 
 @Component({
     templateUrl: './alarmeringen-page.html',
     styleUrls: ['./style.css']
 })
 
-export class AlarmeringenComponent  {
+export class AlarmeringenComponent {
+
+    constructor(private http: Http) {
+
+    }
+
+    result : String = "empty";
+
     alarmeringen = [
         {
-            naam:'Jeroen',
+            persoon:'Jeroen',
             locatie: {
+                straat: "Scheepshellingstraat",
                 lat: 53.103243,
                 long: 7.006474
             },
             melding:'gevallen'
         },
         {
-            naam:'Matthew',
+            persoon:'Matthew',
             locatie: {
+                straat: "Britanniawijk",
                 lat: 53.103043,
                 long: 7.016474
             },
             melding:'gebied verlaten'
         },
         {
-            naam:'Max',
+            persoon:'Max',
             locatie: {
+                straat: "Scheepshellingstraat",
                 lat: 53.103943,
                 long: 7.006574
             },
             melding:'gevallen'
         },
         {
-            naam:'Jeffrey',
+            persoon:'Jeffrey',
             locatie: {
+                straat: "Sebo Ennemaweg",
                 lat: 53.173243,
                 long: 7.006474
             },
