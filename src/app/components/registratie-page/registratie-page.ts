c84bb68cf39f872ebd0a16f292d9ae0bd34aefc5
Jeroenb13 <jeroen.bakker@student.stenden.com>
2017-05-15 10:39:57 +0200

    Registratiescherm met validatie in progress

--- src/app/components/registratie-page/registratie-page.ts
+++ src/app/components/registratie-page/registratie-page.ts
@@ -13,14 +13,15 @@ export class RegistratieComponent  {
     constructor(fb: FormBuilder) {
         this.registrationForm = fb.group({
 
+            //
 
             'firstName' :  [null, Validators.required],
             'surName'  : [null, Validators.required],
-            'eMail'  : [null, Validators.required]
-           /** 'userName' : "",//[null, Validators.required],
-            'passWord' : "",//[null, Validators.required],
-            'repeatpassWord' : ""//[null, Validators.required]
-             */
+            'eMail'  : [null, Validators.compose([Validators.required,  Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)])],
+            'userName' : [null, Validators.compose([])],
+            'passWord' : [null, Validators.required],
+            'repeatpassWord' : [null, Validators.required]
+
         })
     }
     submitForm(value:any){
