8eae9739848258623cd1db3e5ee2fd4d039bdd8b
DaanHaitsma <daan@haitsma-dejager.nl>
2017-05-15 13:13:49 +0200

    alarmeringen

--- src/app/components/alarmeringen-page/alarmeringen-page.ts
+++ src/app/components/alarmeringen-page/alarmeringen-page.ts
@@ -16,40 +16,84 @@ export class AlarmeringenComponent {
 
     alarmeringen = [
         {
-            persoon:'Jeroen',
-            locatie: {
-                straat: "Scheepshellingstraat",
-                lat: 53.103243,
-                long: 7.006474
+            persoon: {
+                naam: 'Max'
             },
-            melding:'gevallen'
+            locatie: {
+                adres: {
+                    straat: "Raadhuisplein",
+                    plaats: "Emmen"
+                },
+                coordinaten: {
+                    lat: 52.782614,
+                    long: 6.89081
+                }
+            },
+            melding: {
+                categorie: "gebied",
+                bericht: "gebied verlaten",
+                datum: "2017-05-03 13:02"
+            }
         },
         {
-            persoon:'Matthew',
-            locatie: {
-                straat: "Britanniawijk",
-                lat: 53.103043,
-                long: 7.016474
+            persoon: {
+                naam: 'Jeroen'
             },
-            melding:'gebied verlaten'
+            locatie: {
+                adres: {
+                    straat: "Scheepshellingstraat",
+                    plaats: "Oude Pekela"
+                },
+                coordinaten: {
+                    lat: 53.103243,
+                    long: 7.006474
+                }
+            },
+            melding: {
+                categorie: "val",
+                bericht: "gevallen",
+                datum: "2017-05-03 13:02"
+            }
         },
         {
-            persoon:'Max',
-            locatie: {
-                straat: "Scheepshellingstraat",
-                lat: 53.103943,
-                long: 7.006574
+            persoon: {
+                naam: 'Matthew'
             },
-            melding:'gevallen'
+            locatie: {
+                adres: {
+                    straat: "Britanniawijk",
+                    plaats: "Oude Pekela"
+                },
+                coordinaten: {
+                    lat: 53.103043,
+                    long: 7.016474
+                }
+            },
+            melding: {
+                categorie: "gebied",
+                bericht: "gebied verlaten",
+                datum: "2017-05-03 13:02"
+            }
         },
         {
-            persoon:'Jeffrey',
-            locatie: {
-                straat: "Sebo Ennemaweg",
-                lat: 53.173243,
-                long: 7.006474
+            persoon: {
+                naam: 'Jeffrey'
             },
-            melding:'gevallen'
-        },
+            locatie: {
+                adres: {
+                    straat: "Sebo Ennemaweg",
+                    plaats: "Midwolda"
+                },
+                coordinaten: {
+                    lat: 53.173243,
+                    long: 7.006474
+                }
+            },
+            melding: {
+                categorie: "val",
+                bericht: "gevallen",
+                datum: "2017-05-03 13:02"
+            }
+        }
     ];
 }
